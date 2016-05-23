9510fbf3ec1bed7d1e97b4a9e96a522df3ba7194
Marian <marian@mds-mac-mini.local>
2016-05-23 14:13:39 +0200

    Initial Commit

---
+++ TryJenkins/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TryJenkins
+//
+//  Created by Marian on 5/23/16.
+//  Copyright © 2016 Marian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
